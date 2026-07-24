3169bfa6f894694a325c34e9f762966ea3a6130b
Parasa Balaji Sri Ram <ramuparasa02@gmail.com>
2026-07-25 00:02:42 +0530

    Update CGPA in Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,7 +8,7 @@ const educationData = [
         title: "B.Tech in Computer Science & Engineering",
         institution: "Lovely Professional University, Punjab",
         duration: "2023 - Present",
-        description: "CGPA : 8.13",
+        description: "CGPA : 8.29",
         icon: <GraduationCap size={18} />
     },
     {
